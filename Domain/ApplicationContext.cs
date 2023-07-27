a84a2d89515c03e54eaecfa7d476cb012a34b6fd
dthema <gleb.info2102@gmail.com>
2023-07-27 17:43:55 +0300

    fix: domain db context

--- Domain/ApplicationContext.cs
+++ Domain/ApplicationContext.cs
@@ -18,27 +18,42 @@ public class ApplicationContext : DbContext
     {
         modelBuilder.Entity<User>()
             .HasKey(x => x.Id);
+        modelBuilder.Entity<User>()
+            .HasIndex(x => x.Login)
+            .IsUnique();
+        modelBuilder.Entity<User>()
+            .HasOne(x => x.Settings);
+
         modelBuilder.Entity<UserSettings>()
             .HasKey(x => x.Id);
+        modelBuilder.Entity<UserSettings>()
+            .HasMany(x => x.Categories)
+            .WithMany();
+
         modelBuilder.Entity<Category>()
             .HasKey(x => x.Id);
+        modelBuilder.Entity<Category>()
+            .HasIndex(x => x.Name)
+            .IsUnique();
+        
         modelBuilder.Entity<Place>()
             .HasKey(x => x.Id);
         modelBuilder.Entity<Place>()
             .OwnsOne(x => x.Location);
-        modelBuilder.Entity<PlacesChain>()
-            .HasKey(x => x.Id);
-        
-        modelBuilder.Entity<User>()
-            .HasOne(x => x.Settings);
-        modelBuilder.Entity<UserSettings>()
-            .HasMany(x => x.Categories);
-
         modelBuilder.Entity<Place>()
             .OwnsOne(x => x.Description);
+        
         modelBuilder.Entity<PlacesChain>()
-            .HasMany(x => x.Places);
+            .HasKey(x => x.Id);
+        modelBuilder.Entity<PlacesChain>()
+            .HasMany(x => x.Places)
+            .WithMany()
+            .UsingEntity(x =>
+            {
+                x.Property<int>("order").IsRequired();
+                x.ToTable("PlacesAndChains");
+            });
         modelBuilder.Entity<PlacesChain>()
             .OwnsOne(x => x.Description);
-    }
+        }
 }
